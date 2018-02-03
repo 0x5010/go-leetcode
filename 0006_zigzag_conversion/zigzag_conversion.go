d545be79d3d9a29d081775c5e14bc6452c4a57d3
0x5010 <0xh4cker@gmail.com>
2018-02-03 18:21:01 +0800

    0006

---
+++ 0006_zigzag_conversion/zigzag_conversion.go
@@ -0,0 +1,35 @@
+package leetcode0006
+
+import (
+	"bytes"
+)
+
+func convert(s string, numRows int) string {
+	if numRows == 1 || len(s) <= numRows {
+		return s
+	}
+
+	res := bytes.Buffer{}
+
+	p := numRows*2 - 2
+
+	for i := 0; i < len(s); i += p {
+		res.WriteByte(s[i])
+	}
+
+	for i := 1; i <= numRows-2; i++ {
+		res.WriteByte(s[i])
+
+		for j := p; j-i < len(s); j += p {
+			res.WriteByte(s[j-i])
+			if i+j < len(s) {
+				res.WriteByte(s[i+j])
+			}
+		}
+	}
+
+	for i := numRows - 1; i < len(s); i += p {
+		res.WriteByte(s[i])
+	}
+	return res.String()
+}
