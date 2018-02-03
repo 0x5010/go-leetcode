ff2fb80c163de0b217405022f79016ddbe9a4a5d
0x5010 <0xh4cker@gmail.com>
2018-02-03 15:57:53 +0800

    add 0004

---
+++ 0004_median_of_two_sorted_arrays/median_of_two_sorted_arrays.go
@@ -0,0 +1,57 @@
+package leetcode0003
+
+func max(x int, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func min(x int, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	m, n := len(nums1), len(nums2)
+	if m > n {
+		nums1, nums2, m, n = nums2, nums1, n, m
+	}
+
+	iMin, iMax, halfLen := 0, m, (m+n+1)/2
+	for iMin <= iMax {
+		i := (iMin + iMax) / 2
+		j := halfLen - i
+		if i < iMax && nums2[j-1] > nums1[i] {
+			iMin++
+		} else if i > iMin && nums1[i-1] > nums2[j] {
+			iMax--
+		} else {
+			maxLeft := 0
+			if i == 0 {
+				maxLeft = nums2[j-1]
+			} else if j == 0 {
+				maxLeft = nums1[i-1]
+			} else {
+				maxLeft = max(nums1[i-1], nums2[j-1])
+			}
+
+			if (m+n)%2 == 1 {
+				return float64(maxLeft)
+			}
+
+			minRight := 0
+			if i == m {
+				minRight = nums2[j]
+			} else if j == n {
+				minRight = nums1[i]
+			} else {
+				minRight = min(nums1[i], nums2[j])
+			}
+			return float64(maxLeft+minRight) / 2.0
+		}
+	}
+	return 0.0
+}
