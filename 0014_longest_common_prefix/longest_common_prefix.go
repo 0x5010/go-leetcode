3448a5f9f05ad62c678c1fb413a1f5d73d66955c
0x5010 <0xh4cker@gmail.com>
2018-02-05 11:57:54 +0800

    0014

---
+++ 0014_longest_common_prefix/longest_common_prefix.go
@@ -0,0 +1,78 @@
+package leetcode0014
+
+import (
+	"bytes"
+)
+
+type trieNode struct {
+	links []*trieNode
+	isEnd bool
+	size  int
+}
+
+func newTrieNode() *trieNode {
+	return &trieNode{
+		links: make([]*trieNode, 26),
+	}
+}
+
+func (tn *trieNode) get(ch byte) *trieNode {
+	return tn.links[ch-'a']
+}
+
+func (tn *trieNode) put(ch byte, node *trieNode) {
+	tn.links[ch-'a'] = node
+	tn.size++
+}
+
+func (tn *trieNode) containsKey(ch byte) bool {
+	return tn.get(ch) != nil
+}
+
+type trie struct {
+	root *trieNode
+}
+
+func newTrie() *trie {
+	return &trie{root: newTrieNode()}
+}
+
+func (t *trie) insert(s string) {
+	node := t.root
+	for _, ch := range []byte(s) {
+		if !node.containsKey(ch) {
+			node.put(ch, newTrieNode())
+		}
+		node = node.get(ch)
+	}
+	node.isEnd = true
+}
+
+func (t *trie) searchLongestPrefix(s string) string {
+	node := t.root
+	prifix := bytes.Buffer{}
+	for _, ch := range []byte(s) {
+		if node.containsKey(ch) && node.size == 1 && !node.isEnd {
+			prifix.WriteByte(ch)
+			node = node.get(ch)
+		} else {
+			break
+		}
+	}
+	return prifix.String()
+}
+
+func longestCommonPrefix(strs []string) string {
+	if strs == nil || len(strs) == 0 {
+		return ""
+	}
+	if len(strs) == 1 {
+		return strs[0]
+	}
+
+	t := newTrie()
+	for _, s := range strs {
+		t.insert(s)
+	}
+	return t.searchLongestPrefix(strs[0])
+}
