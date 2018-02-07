484ecd1137ab37cbd0d2b6023372d1b6b65eea92
0x5010 <0xh4cker@gmail.com>
2018-02-07 10:51:15 +0800

    0021

---
+++ 0021_merge_two_sorted_lists/merge_two_sorted_lists.go
@@ -0,0 +1,26 @@
+package leetcode0021
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = mergeTwoLists(l2.Next, l1)
+		return l2
+	}
+}
