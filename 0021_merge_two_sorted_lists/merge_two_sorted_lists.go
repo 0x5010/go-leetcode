8a4975fa14fa1245cebdf45522ad9de5494acabf
0x5010 <0xh4cker@gmail.com>
2018-02-07 10:51:56 +0800

    0020

--- 0021_merge_two_sorted_lists/merge_two_sorted_lists.go
+++ 0021_merge_two_sorted_lists/merge_two_sorted_lists.go
@@ -19,8 +19,7 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	if l1.Val < l2.Val {
 		l1.Next = mergeTwoLists(l1.Next, l2)
 		return l1
-	} else {
-		l2.Next = mergeTwoLists(l2.Next, l1)
-		return l2
 	}
+	l2.Next = mergeTwoLists(l2.Next, l1)
+	return l2
 }
