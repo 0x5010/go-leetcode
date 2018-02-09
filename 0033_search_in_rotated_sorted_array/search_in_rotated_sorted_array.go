0a0d8bd573b9387579f30be9d1d49aafbe78f821
0x5010 <0xh4cker@gmail.com>
2018-02-09 11:10:26 +0800

    0033

---
+++ 0033_search_in_rotated_sorted_array/search_in_rotated_sorted_array.go
@@ -0,0 +1,29 @@
+package leetcode0033
+
+func search(nums []int, target int) int {
+	index, maxIndex, numsLen := 0, 0, len(nums)
+	if numsLen == 0 {
+		return -1
+	}
+
+	for maxIndex+1 < numsLen && nums[maxIndex] < nums[maxIndex+1] {
+		maxIndex++
+	}
+
+	for l, r := 0, numsLen-1; l <= r; {
+		m := (l + r) / 2
+		index = m + maxIndex + 1
+		if index >= numsLen {
+			index -= numsLen
+		}
+
+		if nums[index] > target {
+			r = m - 1
+		} else if nums[index] < target {
+			l = m + 1
+		} else {
+			return index
+		}
+	}
+	return -1
+}
