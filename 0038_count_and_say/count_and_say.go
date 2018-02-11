802e2cad3d9ab42f5a6e36b89b69b95fddf31cca
0x5010 <0xh4cker@gmail.com>
2018-02-11 16:50:12 +0800

    0038

---
+++ 0038_count_and_say/count_and_say.go
@@ -0,0 +1,29 @@
+package leetcode0038
+
+import (
+	"bytes"
+)
+
+func countAndSay(n int) string {
+	if n == 0 {
+		return ""
+	}
+	res := "1"
+	if n == 1 {
+		return res
+	}
+	for i := 1; i < n; i++ {
+		b := bytes.Buffer{}
+		for j, count := 0, 1; j < len(res); j++ {
+			if j+1 == len(res) || res[j] != res[j+1] {
+				b.WriteByte(byte(count + '0'))
+				b.WriteByte(res[j])
+				count = 1
+			} else {
+				count++
+			}
+		}
+		res = b.String()
+	}
+	return res
+}
