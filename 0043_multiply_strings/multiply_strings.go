7b9e421640a882284f72262c0449e79a6f2b3bb5
0x5010 <0xh4cker@gmail.com>
2018-02-13 18:16:23 +0800

    0043

---
+++ 0043_multiply_strings/multiply_strings.go
@@ -0,0 +1,34 @@
+package leetcode0043
+
+import (
+	"bytes"
+)
+
+func multiply(num1 string, num2 string) string {
+	if num1 == "0" || num2 == "0" {
+		return "0"
+	}
+	len1, len2 := len(num1), len(num2)
+	product := make([]int, len1+len2)
+
+	for i := 0; i < len1; i++ {
+		for j := 0; j < len2; j++ {
+			product[i+j+1] += int(num1[i]-'0') * int(num2[j]-'0')
+		}
+	}
+
+	for i := len(product) - 1; i > 0; i-- {
+		product[i-1] += product[i] / 10
+		product[i] = product[i] % 10
+	}
+
+	if product[0] == 0 {
+		product = product[1:]
+	}
+
+	res := bytes.Buffer{}
+	for _, i := range product {
+		res.WriteByte('0' + byte(i))
+	}
+	return res.String()
+}
