3c859da8c6c0bf432392c78462c166b88802a778
0x5010 <0xh4cker@gmail.com>
2018-02-13 19:08:15 +0800

    0044

---
+++ 0044_wildcard_matching/wildcard_matching.go
@@ -0,0 +1,27 @@
+package leetcode0044
+
+func isMatch(s string, p string) bool {
+	sLen, pLen := len(s), len(p)
+
+	dp := make([][]bool, sLen+1)
+	for i := 0; i < sLen+1; i++ {
+		dp[i] = make([]bool, pLen+1)
+	}
+
+	dp[0][0] = true
+	for i := 1; i <= pLen; i++ {
+		if p[i-1] == '*' {
+			dp[0][i] = dp[0][i-1]
+		}
+	}
+	for i := 1; i <= sLen; i++ {
+		for j := 1; j <= pLen; j++ {
+			if p[j-1] != '*' {
+				dp[i][j] = (p[j-1] == s[i-1] || p[j-1] == '?') && dp[i-1][j-1]
+			} else {
+				dp[i][j] = dp[i-1][j] || dp[i][j-1]
+			}
+		}
+	}
+	return dp[sLen][pLen]
+}
