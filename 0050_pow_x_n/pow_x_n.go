414ae04f0355aaa9834bff0af024630ae129b69f
0x5010 <0xh4cker@gmail.com>
2018-02-14 13:00:48 +0800

    0050

---
+++ 0050_pow_x_n/pow_x_n.go
@@ -0,0 +1,23 @@
+package leetcode0050
+
+func myPow(x float64, n int) float64 {
+	if n < 0 {
+		return 1.0 / pow(x, -n)
+	}
+	return pow(x, n)
+}
+
+func pow(x float64, n int) float64 {
+	if x == 0 {
+		return 0
+	}
+	if n == 0 {
+		return 1
+	}
+
+	res := pow(x, n>>1)
+	if n&1 == 0 {
+		return res * res
+	}
+	return res * res * x
+}
