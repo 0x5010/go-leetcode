7a7b47d20659b6a980263530c4ed54f52c08ec55
0x5010 <0xh4cker@gmail.com>
2018-02-22 19:14:15 +0800

    0069

---
+++ 0069_sqrt_x/sqrt_x.go
@@ -0,0 +1,23 @@
+package leetcode0069
+
+func mySqrt(x int) int {
+	if x <= 1 {
+		return x
+	}
+
+	low := 1
+	high := x
+	for low < high {
+		mid := (low + high) / 2
+		sq := mid * mid
+		if sq > x {
+			high = mid
+		} else if sq < x {
+			low = mid + 1
+		} else {
+			return mid
+		}
+	}
+
+	return high - 1
+}
