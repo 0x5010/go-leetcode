39fb85c3ecb4ba4724c20d6887f4059fc8619a78
0x5010 <0xh4cker@gmail.com>
2018-02-22 19:06:34 +0800

    0068

---
+++ 0068_text_justification/text_justification.go
@@ -0,0 +1,35 @@
+package leetcode0068
+
+import (
+	"strings"
+)
+
+func fullJustify(words []string, maxWidth int) []string {
+	res := []string{}
+
+	for i, w := 0, 0; i < len(words); i = w {
+		l := -1
+		for w = i; w < len(words) && l+len(words[w])+1 <= maxWidth; w++ {
+			l += len(words[w]) + 1
+		}
+
+		tmp := []string{words[i]}
+		space, extra := 1, 0
+		if w != i+1 && w != len(words) {
+			space = (maxWidth-l)/(w-i-1) + 1
+			extra = (maxWidth - l) % (w - i - 1)
+		}
+		for j := i + 1; j < w; j++ {
+			tmp = append(tmp, strings.Repeat(" ", space))
+			if extra > 0 {
+				tmp = append(tmp, " ")
+				extra--
+			}
+			tmp = append(tmp, words[j])
+		}
+		str := strings.Join(tmp, "")
+		str += strings.Repeat(" ", maxWidth-len(str))
+		res = append(res, str)
+	}
+	return res
+}
