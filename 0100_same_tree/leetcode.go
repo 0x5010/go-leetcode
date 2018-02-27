c61df95eb842be9b99e3c146d44a64e920f0b387
0x5010 <0xh4cker@gmail.com>
2018-02-27 20:14:06 +0800

    0100

---
+++ 0100_same_tree/leetcode.go
@@ -0,0 +1,7 @@
+package leetcode00100
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
---
+++ 0100_same_tree/same_tree.go
@@ -0,0 +1,21 @@
+package leetcode00100
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	return p.Val == q.Val &&
+		isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
