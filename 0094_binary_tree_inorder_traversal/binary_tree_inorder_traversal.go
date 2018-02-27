12f6876bcc7aeb95641abfe7bc4fbfe2728901e8
0x5010 <0xh4cker@gmail.com>
2018-02-27 16:19:44 +0800

    0094

---
+++ 0094_binary_tree_inorder_traversal/binary_tree_inorder_traversal.go
@@ -0,0 +1,25 @@
+package leetcode0094
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	if root.Left == nil && root.Right == nil {
+		return []int{root.Val}
+	}
+
+	res := inorderTraversal(root.Left)
+	res = append(res, root.Val)
+	res = append(res, inorderTraversal(root.Right)...)
+	return res
+}
