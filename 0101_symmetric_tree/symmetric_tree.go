d7deea28c92b3fc74fe5ed241673675fc237f0df
0x5010 <0xh4cker@gmail.com>
2018-02-28 12:03:03 +0800

    0101

---
+++ 0101_symmetric_tree/symmetric_tree.go
@@ -0,0 +1,28 @@
+package leetcode00101
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return symmetric(root.Left, root.Right)
+}
+
+func symmetric(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil || right == nil {
+		return false
+	}
+	return left.Val == right.Val &&
+		symmetric(left.Left, right.Right) && symmetric(left.Right, right.Left)
+}
