c40bbb71fe710ce483a6ec719716be3910bcb43d
0x5010 <0xh4cker@gmail.com>
2018-02-28 14:30:02 +0800

    0102

---
+++ 0102_binary_tree_level_order_traversal/binary_tree_level_order_traversal.go
@@ -0,0 +1,34 @@
+package leetcode00102
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+	s := []*TreeNode{root}
+	res := [][]int{}
+	for len(s) != 0 {
+		tmp := []*TreeNode{}
+		l := []int{}
+		for _, node := range s {
+			l = append(l, node.Val)
+			if node.Left != nil {
+				tmp = append(tmp, node.Left)
+			}
+			if node.Right != nil {
+				tmp = append(tmp, node.Right)
+			}
+		}
+		res = append(res, l)
+		s = tmp
+	}
+	return res
+}
