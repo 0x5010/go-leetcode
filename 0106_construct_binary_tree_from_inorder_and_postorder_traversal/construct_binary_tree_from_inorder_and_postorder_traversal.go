b9c2ef7d247990edf1d29fb48690134a46bc31fd
0x5010 <0xh4cker@gmail.com>
2018-03-01 10:43:50 +0800

    0106

---
+++ 0106_construct_binary_tree_from_inorder_and_postorder_traversal/construct_binary_tree_from_inorder_and_postorder_traversal.go
@@ -0,0 +1,33 @@
+package leetcode00106
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func buildTree(inorder []int, postorder []int) *TreeNode {
+	if len(inorder) == 0 {
+		return nil
+	}
+
+	res := &TreeNode{Val: postorder[len(postorder)-1]}
+	if len(inorder) == 1 {
+		return res
+	}
+
+	index := 0
+	for i, v := range inorder {
+		if v == res.Val {
+			index = i
+			break
+		}
+	}
+
+	res.Left = buildTree(inorder[:index], postorder[:index])
+	res.Right = buildTree(inorder[index+1:], postorder[index:len(postorder)-1])
+	return res
+}
---
+++ 0106_construct_binary_tree_from_inorder_and_postorder_traversal/leetcode.go
@@ -0,0 +1,7 @@
+package leetcode00106
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
