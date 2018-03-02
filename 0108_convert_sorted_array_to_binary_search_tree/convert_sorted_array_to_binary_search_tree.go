d8ab6273eb8c12b530d19e8329f1c2d6d308e8b6
0x5010 <0xh4cker@gmail.com>
2018-03-02 10:32:35 +0800

    0108

---
+++ 0108_convert_sorted_array_to_binary_search_tree/convert_sorted_array_to_binary_search_tree.go
@@ -0,0 +1,23 @@
+package leetcode00108
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	middle := len(nums) / 2
+	return &TreeNode{
+		Val:   nums[middle],
+		Left:  sortedArrayToBST(nums[:middle]),
+		Right: sortedArrayToBST(nums[middle+1:]),
+	}
+}
