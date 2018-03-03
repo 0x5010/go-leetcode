0787874a128a581671b6481585966766209f00ca
0x5010 <0xh4cker@gmail.com>
2018-03-03 17:37:09 +0800

    0112

---
+++ 0112_path_sum/path_sum.go
@@ -0,0 +1,23 @@
+package leetcode00112
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func hasPathSum(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+
+	sum -= root.Val
+	if root.Left == nil && root.Right == nil {
+		return sum == 0
+	}
+
+	return hasPathSum(root.Left, sum) || hasPathSum(root.Right, sum)
+}
