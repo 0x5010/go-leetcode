0d71a2bf29edc801e5ac1cadf0ce88b0e55c7b6d
0x5010 <0xh4cker@gmail.com>
2018-03-03 18:17:22 +0800

    0113

---
+++ 0113_path_sum_ii/path_sum_ii.go
@@ -0,0 +1,35 @@
+package leetcode00113
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func pathSum(root *TreeNode, sum int) [][]int {
+	res := [][]int{}
+	var recur func(*TreeNode, int, []int)
+	recur = func(node *TreeNode, sum int, path []int) {
+		if node == nil {
+			return
+		}
+		sum -= node.Val
+		path = append(path, node.Val)
+		if node.Left == nil && node.Right == nil {
+			if sum == 0 {
+				tmp := make([]int, len(path))
+				copy(tmp, path)
+				res = append(res, tmp)
+				return
+			}
+		}
+		recur(node.Left, sum, path)
+		recur(node.Right, sum, path)
+
+	}
+	recur(root, sum, []int{})
+	return res
+}
