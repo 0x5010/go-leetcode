1a31efcfd3303928dfdce0bb4000558d0efe95d6
0x5010 <0xh4cker@gmail.com>
2018-03-03 19:07:30 +0800

    0114

---
+++ 0114_flatten_binary_tree_to_linked_list/flatten_binary_tree_to_linked_list.go
@@ -0,0 +1,33 @@
+package leetcode00114
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func flatten(root *TreeNode) {
+	var prev *TreeNode
+	recur(root, &prev)
+}
+
+func recur(node *TreeNode, prev **TreeNode) {
+	if node == nil {
+		return
+	}
+	if *prev != nil {
+		(*prev).Left = nil
+		(*prev).Right = node
+	}
+	*prev = node
+	l, r := node.Left, node.Right
+	if l != nil {
+		recur(l, prev)
+	}
+	if r != nil {
+		recur(r, prev)
+	}
+}
---
+++ 0114_flatten_binary_tree_to_linked_list/leetcode.go
@@ -0,0 +1,7 @@
+package leetcode00114
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
