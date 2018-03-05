833cc9740581c970c65cda3dc29e535c5f91c61e
0x5010 <0xh4cker@gmail.com>
2018-03-05 11:12:39 +0800

    0120

---
+++ 0121_best_time_to_buy_and_sell_stock/best_time_to_buy_and_sell_stock.go
@@ -0,0 +1,18 @@
+package leetcode00121
+
+func maxProfit(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+	max, minElement := 0, prices[0]
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < minElement {
+			minElement = prices[i]
+		}
+		tmp := prices[i] - minElement
+		if tmp > max {
+			max = tmp
+		}
+	}
+	return max
+}
