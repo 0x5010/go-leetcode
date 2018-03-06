14dfec4706e8f65f4a16a60f7fbc92ee27ab5e77
0x5010 <0xh4cker@gmail.com>
2018-03-06 10:50:20 +0800

    0122

---
+++ 0122_best_time_to_buy_and_sell_stock_ii/best_time_to_buy_and_sell_stock_ii.go
@@ -0,0 +1,14 @@
+package leetcode00122
+
+func maxProfit(prices []int) int {
+	res := 0
+	for i, p := range prices {
+		if i == 0 {
+			continue
+		}
+		if p > prices[i-1] {
+			res += p - prices[i-1]
+		}
+	}
+	return res
+}
