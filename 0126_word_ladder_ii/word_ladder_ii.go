527a58f043a720801d21bfcfa9126f9cb3812b6f
0x5010 <0xh4cker@gmail.com>
2018-03-08 14:50:24 +0800

    0126

---
+++ 0126_word_ladder_ii/word_ladder_ii.go
@@ -0,0 +1,106 @@
+package leetcode0126
+
+var (
+	exists = struct{}{}
+)
+
+func findLadders(beginWord string, endWord string, wordList []string) [][]string {
+	paths := [][]string{}
+	path := []string{beginWord}
+	if beginWord == endWord {
+		paths = append(paths, path)
+		return paths
+	}
+
+	wordSet, forward, backward := set(), set(), set()
+
+	for _, w := range wordList {
+		wordSet[w] = exists
+	}
+	if _, ok := wordSet[endWord]; !ok {
+		return paths
+	}
+	forward[beginWord] = exists
+	backward[endWord] = exists
+	tree := make(map[string][]string)
+
+	var dfs func(string, string, map[string][]string, []string)
+	dfs = func(
+		beginWord, endWord string,
+		tree map[string][]string,
+		path []string,
+	) {
+		if beginWord == endWord {
+			tmp := make([]string, len(path))
+			copy(tmp, path)
+			paths = append(paths, tmp)
+			return
+		}
+		for _, it := range tree[beginWord] {
+			path = append(path, it)
+			dfs(it, endWord, tree, path)
+			path = path[:len(path)-1]
+		}
+	}
+
+	if buildTree(wordSet, forward, backward, tree, false) {
+		dfs(beginWord, endWord, tree, path)
+	}
+	return paths
+}
+
+func set() map[string]struct{} {
+	return make(map[string]struct{})
+}
+
+func buildTree(
+	wordSet, forward, backward map[string]struct{},
+	tree map[string][]string,
+	reversed bool,
+) bool {
+	if len(forward) == 0 {
+		return false
+	}
+	if len(forward) > len(backward) {
+		return buildTree(wordSet, backward, forward, tree, !reversed)
+	}
+	for k := range forward {
+		delete(wordSet, k)
+	}
+	for k := range backward {
+		delete(wordSet, k)
+	}
+	next := set()
+	done := false
+	for it := range forward {
+		word := []byte(it)
+		for i := 0; i < len(word); i++ {
+			c := word[i]
+			for word[i] = 'a'; word[i] <= 'z'; word[i]++ {
+				if word[i] == c {
+					continue
+				}
+				s := string(word)
+				if _, ok := backward[s]; ok {
+					done = true
+					if reversed {
+						tree[s] = append(tree[s], it)
+					} else {
+						tree[it] = append(tree[it], s)
+					}
+				} else if !done {
+					if _, ok := wordSet[s]; ok {
+						next[s] = exists
+						if reversed {
+							tree[s] = append(tree[s], it)
+						} else {
+							tree[it] = append(tree[it], s)
+						}
+					}
+				}
+			}
+			word[i] = c
+		}
+	}
+	return done || buildTree(wordSet, next, backward, tree, reversed)
+}
