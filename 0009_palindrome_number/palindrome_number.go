534704a40e78965676fa312ab7db69526614cda5
0x5010 <0xh4cker@gmail.com>
2018-03-10 20:19:48 +0800

    0009

--- 0009_palindrome_number/palindrome_number.go
+++ 0009_palindrome_number/palindrome_number.go
@@ -1,19 +1,13 @@
 package leetcode0009
 
-import (
-	"strconv"
-)
-
 func isPalindrome(x int) bool {
-	if x < 0 {
+	if x < 0 || (x != 0 && x%10 == 0) {
 		return false
 	}
-
-	s := strconv.Itoa(x)
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		if s[i] != s[j] {
-			return false
-		}
+	reverse := 0
+	for x > reverse {
+		reverse = reverse*10 + (x % 10)
+		x = x / 10
 	}
-	return true
+	return x == reverse || x == reverse/10
 }
