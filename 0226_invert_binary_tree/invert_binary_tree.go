5afc1f187148031e44c5859a72a322832df1acde
0x5010 <0xh4cker@gmail.com>
2018-03-26 23:04:36 +0800

    0226

---
+++ 0226_invert_binary_tree/invert_binary_tree.go
@@ -0,0 +1,18 @@
+package leetcode0226
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
+	return root
+}
---
+++ 0226_invert_binary_tree/leetcode.go
@@ -0,0 +1,7 @@
+package leetcode0226
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
