ff7492a8bc2acd41b23d41cffa34250ae5fbd75e
0x5010 <0xh4cker@gmail.com>
2018-03-30 11:15:51 +0800

    0240

---
+++ 0240_search_a_2d_matrix_ii/search_a_2d_matrix_ii.go
@@ -0,0 +1,21 @@
+package leetcode0240
+
+func searchMatrix(matrix [][]int, target int) bool {
+	m := len(matrix)
+	if m == 0 {
+		return false
+	}
+	n := len(matrix[0])
+
+	i, j := m-1, 0
+	for 0 <= i && j < n {
+		if matrix[i][j] == target {
+			return true
+		} else if matrix[i][j] < target {
+			j++
+		} else {
+			i--
+		}
+	}
+	return false
+}
