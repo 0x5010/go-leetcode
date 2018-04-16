b62d60a345abdf2244e09cb03c90be668951129e
0x5010 <0xh4cker@gmail.com>
2018-04-16 22:23:31 +0800

    0319

---
+++ 0319_bulb_switcher/bulb_switcher.go
@@ -0,0 +1,27 @@
+package leetcode0319
+
+func bulbSwitch(n int) int {
+	return mySqrt(n)
+}
+
+func mySqrt(x int) int {
+	if x <= 1 {
+		return x
+	}
+
+	low := 1
+	high := x
+	for low < high {
+		mid := (low + high) / 2
+		sq := mid * mid
+		if sq > x {
+			high = mid
+		} else if sq < x {
+			low = mid + 1
+		} else {
+			return mid
+		}
+	}
+
+	return high - 1
+}
