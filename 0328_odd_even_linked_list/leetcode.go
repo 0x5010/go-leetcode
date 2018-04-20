096736d768c46e6be98d8bca66f4e92e9e6523ed
0x5010 <0xh4cker@gmail.com>
2018-04-20 23:19:41 +0800

    0328

---
+++ 0328_odd_even_linked_list/leetcode.go
@@ -0,0 +1,6 @@
+package leetcode0328
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
---
+++ 0328_odd_even_linked_list/odd_even_linked_list.go
@@ -0,0 +1,28 @@
+package leetcode0328
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func oddEvenList(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	odd := head
+	even := head.Next
+	evenHead := even
+
+	for even != nil && even.Next != nil {
+		odd.Next = odd.Next.Next
+		even.Next = even.Next.Next
+		odd = odd.Next
+		even = even.Next
+	}
+	odd.Next = evenHead
+	return head
+}
