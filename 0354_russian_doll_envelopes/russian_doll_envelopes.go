5995cbd6ac5da9adc8388b8b6efc3bb13f1aa865
0x5010 <0xh4cker@gmail.com>
2018-04-27 20:10:04 +0800

    0354

---
+++ 0354_russian_doll_envelopes/russian_doll_envelopes.go
@@ -0,0 +1,34 @@
+package leetcode0354
+
+import "sort"
+
+func maxEnvelopes(envelopes [][]int) int {
+	n := len(envelopes)
+	if n < 2 {
+		return n
+	}
+
+	sortBySize(envelopes)
+
+	dp := make([]int, n)
+	res := 1
+	for i, envelope := range envelopes {
+		index := sort.Search(res, func(idx int) bool {
+			return envelopes[dp[idx]][1] >= envelope[1]
+		})
+		dp[index] = i
+		if index >= res {
+			res++
+		}
+	}
+	return res
+}
+
+func sortBySize(envelopes [][]int) {
+	sort.Slice(envelopes, func(i, j int) bool {
+		if envelopes[i][0] == envelopes[j][0] {
+			return envelopes[i][1] > envelopes[j][1]
+		}
+		return envelopes[i][0] < envelopes[j][0]
+	})
+}
