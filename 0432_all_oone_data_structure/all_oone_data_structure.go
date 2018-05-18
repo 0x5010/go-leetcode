6417def15fa93e2b324812e71ec92ed406bfd08a
0x5010 <0xh4cker@gmail.com>
2018-05-18 11:35:47 +0800

    0432

--- 0432_all_oone_data_structure/all_oone_data_structure.go
+++ 0432_all_oone_data_structure/all_oone_data_structure.go
@@ -1,117 +1,92 @@
 package leetcode0432
 
-import "container/heap"
+import (
+	"container/list"
+)
 
 type AllOne struct {
-	m    map[string]*entry
-	maxh maxHeap
-	minh minHeap
+	m map[string]*list.Element
+	l *list.List
 }
 
 /** Initialize your data structure here. */
 func Constructor() AllOne {
-	return AllOne{m: make(map[string]*entry),
-		maxh: maxHeap{},
-		minh: minHeap{},
+	return AllOne{
+		m: make(map[string]*list.Element),
+		l: list.New(),
 	}
 }
 
 /** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
 func (this *AllOne) Inc(key string) {
-	if e, ok := this.m[key]; ok {
-		e.v++
-		heap.Fix(&this.maxh, e.maxIndex)
-		heap.Fix(&this.minh, e.minIndex)
+	e, ok := this.m[key]
+	if !ok {
+		this.m[key] = this.l.PushFront(&entry{
+			k: key,
+			v: 1,
+		})
+		return
+	}
+
+	ele := e.Value.(*entry)
+	ele.v++
+	next := e
+	for next != nil && next.Value.(*entry).v <= ele.v {
+		next = next.Next()
+	}
+	if next == nil {
+		this.l.MoveToBack(e)
 	} else {
-		e = &entry{k: key, v: 1}
-		this.m[key] = e
-		heap.Push(&this.maxh, e)
-		heap.Push(&this.minh, e)
+		this.l.MoveBefore(e, next)
 	}
 }
 
 /** Decrements an existing key by 1. If Key's value is 1, remove it from the data structure. */
 func (this *AllOne) Dec(key string) {
-	if e, ok := this.m[key]; !ok {
+	e, ok := this.m[key]
+	if !ok {
 		return
-	} else if e.v == 1 {
+	}
+
+	ele := e.Value.(*entry)
+	if ele.v == 1 {
 		delete(this.m, key)
-		heap.Remove(&this.maxh, e.maxIndex)
-		heap.Remove(&this.minh, e.minIndex)
+		this.l.Remove(e)
+		return
+	}
+	ele.v--
+	prev := e
+	for prev != nil && prev.Value.(*entry).v >= ele.v {
+		prev = prev.Prev()
+	}
+	if prev == nil {
+		this.l.MoveToFront(e)
 	} else {
-		e.v--
-		heap.Fix(&this.maxh, e.maxIndex)
-		heap.Fix(&this.minh, e.minIndex)
+		this.l.MoveAfter(e, prev)
 	}
 }
 
 /** Returns one of the keys with maximal value. */
 func (this *AllOne) GetMaxKey() string {
-	if len(this.maxh) == 0 {
-		return ""
+	tail := this.l.Back()
+	if tail != nil {
+		return tail.Value.(*entry).k
 	}
-	return this.maxh[0].k
+	return ""
 }
 
 /** Returns one of the keys with Minimal value. */
 func (this *AllOne) GetMinKey() string {
-	if len(this.minh) == 0 {
-		return ""
+	head := this.l.Front()
+	if head != nil {
+		return head.Value.(*entry).k
 	}
-	return this.minh[0].k
+	return ""
 }
 
 type entry struct {
-	k        string
-	v        int
-	maxIndex int
-	minIndex int
-}
-
-type maxHeap []*entry
-
-func (mh maxHeap) Len() int           { return len(mh) }
-func (mh maxHeap) Less(i, j int) bool { return mh[i].v > mh[j].v }
-func (mh maxHeap) Swap(i, j int) {
-	mh[i], mh[j] = mh[j], mh[i]
-	mh[i].maxIndex, mh[j].maxIndex = i, j
-}
-
-func (mh *maxHeap) Push(x interface{}) {
-	e := x.(*entry)
-	e.maxIndex = len(*mh)
-	*mh = append(*mh, e)
-}
-
-func (mh *maxHeap) Pop() interface{} {
-	old := *mh
-	n := len(old)
-	e := old[n-1]
-	*mh = old[:n-1]
-	return e
-}
-
-type minHeap []*entry
-
-func (mh minHeap) Len() int           { return len(mh) }
-func (mh minHeap) Less(i, j int) bool { return mh[i].v < mh[j].v }
-func (mh minHeap) Swap(i, j int) {
-	mh[i], mh[j] = mh[j], mh[i]
-	mh[i].minIndex, mh[j].minIndex = i, j
-}
-
-func (mh *minHeap) Push(x interface{}) {
-	e := x.(*entry)
-	e.minIndex = len(*mh)
-	*mh = append(*mh, e)
-}
-
-func (mh *minHeap) Pop() interface{} {
-	old := *mh
-	n := len(old)
-	e := old[n-1]
-	*mh = old[:n-1]
-	return e
+	k string
+	v int
 }
 
 /**
