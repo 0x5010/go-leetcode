2d21f57397922a217371a04c58e9c8b5988f18d1
0x5010 <0xh4cker@gmail.com>
2018-06-11 16:58:08 +0800

    0451

---
+++ 0451_sort_characters_by_frequency/sort_characters_by_frequency.go
@@ -0,0 +1,39 @@
+package leetcode0451
+
+import "sort"
+
+func frequencySort(s string) string {
+	n := len(s)
+	freqs := make([]freq, 128)
+	for i := 0; i < n; i++ {
+		b := s[i]
+		f := freqs[b]
+		f.b = b
+		f.f++
+		freqs[b] = f
+	}
+	sortByLen(freqs)
+	res := make([]byte, n)
+	i := 0
+	for _, f := range freqs {
+		if f.f == 0 {
+			break
+		}
+		for j := 0; j < f.f; j++ {
+			res[i] = f.b
+			i++
+		}
+	}
+	return string(res)
+}
+
+type freq struct {
+	b byte
+	f int
+}
+
+func sortByLen(freqs []freq) {
+	sort.Slice(freqs, func(i, j int) bool {
+		return freqs[i].f > freqs[j].f
+	})
+}
