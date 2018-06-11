9433e516c20ecf536fe9445be10a3db281e12293
0x5010 <0xh4cker@gmail.com>
2018-06-11 16:34:25 +0800

    0450

---
+++ 0450_delete_node_in_a_bst/delete_node_in_a_bst.go
@@ -0,0 +1,34 @@
+package leetcode0450
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func deleteNode(root *TreeNode, key int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if key < root.Val {
+		root.Left = deleteNode(root.Left, key)
+	} else if key > root.Val {
+		root.Right = deleteNode(root.Right, key)
+	} else {
+		if root.Left == nil {
+			return root.Right
+		} else if root.Right == nil {
+			return root.Left
+		}
+		node := root.Right
+		for node.Left != nil {
+			node = node.Left
+		}
+		root.Val = node.Val
+		root.Right = deleteNode(root.Right, root.Val)
+	}
+	return root
+}
