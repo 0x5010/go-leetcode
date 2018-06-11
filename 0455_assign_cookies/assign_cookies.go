da431114857935987e23f0b00aef92c975949d89
0x5010 <0xh4cker@gmail.com>
2018-06-11 17:54:07 +0800

    0455

---
+++ 0455_assign_cookies/assign_cookies.go
@@ -0,0 +1,22 @@
+package leetcode455
+
+import (
+	"sort"
+)
+
+func findContentChildren(g []int, s []int) int {
+	n := len(g)
+	sort.Ints(g)
+	sort.Ints(s)
+
+	res := 0
+	for _, x := range s {
+		if res >= n {
+			break
+		}
+		if x >= g[res] {
+			res++
+		}
+	}
+	return res
+}
