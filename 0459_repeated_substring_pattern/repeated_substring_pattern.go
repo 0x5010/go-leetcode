54190de1444cd2a1c29d8a0584af1408ee2ed1b4
0x5010 <0xh4cker@gmail.com>
2018-06-12 10:27:16 +0800

    0459

---
+++ 0459_repeated_substring_pattern/repeated_substring_pattern.go
@@ -0,0 +1,11 @@
+package leetcode459
+
+import "strings"
+
+func repeatedSubstringPattern(s string) bool {
+	n := len(s)
+	if n == 0 {
+		return false
+	}
+	return strings.Contains((s + s)[1:n*2-1], s)
+}
