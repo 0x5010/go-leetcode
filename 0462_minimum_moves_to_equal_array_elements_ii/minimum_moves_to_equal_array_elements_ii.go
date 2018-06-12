3431799047eef6225bf1eb243d407cd560642133
0x5010 <0xh4cker@gmail.com>
2018-06-12 11:14:04 +0800

    0462

---
+++ 0462_minimum_moves_to_equal_array_elements_ii/minimum_moves_to_equal_array_elements_ii.go
@@ -0,0 +1,16 @@
+package leetcode462
+
+import "sort"
+
+func minMoves2(nums []int) int {
+	n := len(nums)
+	sort.Ints(nums)
+
+	res := 0
+	for i, j := 0, n-1; i < j; {
+		res += nums[j] - nums[i]
+		i++
+		j--
+	}
+	return res
+}
