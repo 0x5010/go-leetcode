578d2eeb1a04a7c8ee936b2dc330c0d05eee3668
0x5010 <0xh4cker@gmail.com>
2018-06-16 17:35:58 +0800

    0464

---
+++ 0464_can_i_win/can_i_win.go
@@ -0,0 +1,47 @@
+package leetcode464
+
+func canIWin(maxChoosableInteger int, desiredTotal int) bool {
+	sum := (1 + maxChoosableInteger) * maxChoosableInteger / 2
+	if desiredTotal > sum {
+		return false
+	}
+
+	bit := make([]int, maxChoosableInteger+1)
+	for i := maxChoosableInteger; i > 0; i-- {
+		bit[i] = 1 << uint8(i)
+	}
+
+	dp := make(map[int]bool, maxChoosableInteger*maxChoosableInteger)
+
+	var dfs func(int, int) bool
+	dfs = func(usedBit, remains int) bool {
+		if res, ok := dp[usedBit]; ok {
+			return res
+		}
+		if remains <= maxChoosableInteger {
+			for i := maxChoosableInteger; i >= remains; i-- {
+				if usedBit&bit[i] == 0 {
+					dp[usedBit] = true
+					return true
+				}
+			}
+		}
+		for i := maxChoosableInteger; i > 0; i-- {
+			if usedBit&bit[i] > 0 {
+				continue
+			}
+			tmp := usedBit
+			usedBit |= bit[i]
+			lose := dfs(usedBit, remains-i)
+			usedBit = tmp
+
+			if !lose {
+				dp[usedBit] = true
+				return true
+			}
+		}
+		dp[usedBit] = false
+		return false
+	}
+	return dfs(0, desiredTotal)
+}
