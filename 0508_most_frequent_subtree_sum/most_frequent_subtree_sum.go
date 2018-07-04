e08ecbd61abe0b38fff98aa6136eece7ff1e9cd3
0x5010 <0xh4cker@gmail.com>
2018-07-04 11:00:08 +0800

    0508

---
+++ 0508_most_frequent_subtree_sum/most_frequent_subtree_sum.go
@@ -0,0 +1,39 @@
+package leetcode0508
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func findFrequentTreeSum(root *TreeNode) []int {
+	m := map[int]int{}
+	max := 0
+
+	var postorder func(*TreeNode) int
+	postorder = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+		l, r := postorder(node.Left), postorder(node.Right)
+		sum := node.Val + l + r
+		m[sum]++
+		if m[sum] > max {
+			max = m[sum]
+		}
+		return sum
+	}
+
+	postorder(root)
+
+	res := []int{}
+	for sum, count := range m {
+		if count == max {
+			res = append(res, sum)
+		}
+	}
+	return res
+}
