ccb03442f1952cde273c46246d438360e4c76779
0x5010 <0xh4cker@gmail.com>
2018-07-18 10:29:04 +0800

    0598

---
+++ 0598_range_addition_ii/range_addition_ii.go
@@ -0,0 +1,13 @@
+package leetcode0598
+
+func maxCount(m int, n int, ops [][]int) int {
+	for _, op := range ops {
+		if op[0] < m {
+			m = op[0]
+		}
+		if op[1] < n {
+			n = op[1]
+		}
+	}
+	return m * n
+}
