074547e541636c51eda3790a900c70fbff8f53f0
0x5010 <0xh4cker@gmail.com>
2018-07-20 11:11:51 +0800

    0623

---
+++ 0632_smallest_range/smallest_range.go
@@ -0,0 +1,73 @@
+package leetcode0632
+
+import "container/heap"
+
+func smallestRange(nums [][]int) []int {
+	n := len(nums)
+	h := &itemHeap{}
+
+	arrPtrs := make([]int, len(nums))
+
+	max := 0
+	idx := 0
+	for idx < n {
+		arr := nums[idx]
+		arrPtr := arrPtrs[idx]
+		v := arr[arrPtr]
+		if v > max {
+			max = v
+		}
+		heap.Push(h, item{val: v, kth: idx})
+		arrPtrs[idx] = arrPtr + 1
+		idx++
+	}
+
+	minDiff := max - (*h)[0].val
+	res := []int{(*h)[0].val, max}
+
+	for {
+		minPop := heap.Pop(h).(item)
+		arr := nums[minPop.kth]
+		arrPtr := arrPtrs[minPop.kth]
+		if arrPtr >= len(arr) {
+			break
+		}
+		v := arr[arrPtr]
+		if v > max {
+			max = v
+		}
+		heap.Push(h, item{val: v, kth: minPop.kth})
+		arrPtrs[minPop.kth] = arrPtr + 1
+
+		curDiff := max - (*h)[0].val
+		if curDiff < minDiff {
+			minDiff = curDiff
+			res = []int{(*h)[0].val, max}
+		}
+	}
+	return res
+}
+
+type item struct {
+	val int
+	kth int
+}
+
+type itemHeap []item
+
+func (q itemHeap) Less(i, j int) bool { return q[i].val < q[j].val }
+func (q itemHeap) Len() int           { return len(q) }
+func (q itemHeap) Swap(i, j int)      { q[i], q[j] = q[j], q[i] }
+
+func (q *itemHeap) Push(x interface{}) {
+	val := x.(item)
+	*q = append(*q, val)
+}
+
+func (q *itemHeap) Pop() interface{} {
+	old := *q
+	n := len(old)
+	x := old[n-1]
+	*q = old[:n-1]
+	return x
+}
