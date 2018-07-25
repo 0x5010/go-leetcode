ddbeeba503d89004b1982efc1fe23d1c492613cd
0x5010 <0xh4cker@gmail.com>
2018-07-25 14:11:41 +0800

    0667

---
+++ 0667_beautiful_arrangement_ii/beautiful_arrangement_ii.go
@@ -0,0 +1,19 @@
+package leetcode0667
+
+func constructArray(n int, k int) []int {
+	res := make([]int, 0, n)
+	i, j := 1, n
+	for i <= j {
+		if k%2 == 1 {
+			res = append(res, i)
+			i++
+		} else {
+			res = append(res, j)
+			j--
+		}
+		if k > 1 {
+			k--
+		}
+	}
+	return res
+}
