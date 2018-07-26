43674dcc8432a739411904f43c3b58e0ba9218f1
0x5010 <0xh4cker@gmail.com>
2018-07-26 16:54:54 +0800

    0673

--- 0673_number_of_longest_increasing_subsequence/number_of_longest_increasing_subsequence.go
+++ 0673_number_of_longest_increasing_subsequence/number_of_longest_increasing_subsequence.go
@@ -2,26 +2,54 @@ package leetcode0673
 
 func findNumberOfLIS(nums []int) int {
 	n := len(nums)
-	max := 0
-	res := 0
-	dp := make([][2]int, n)
-	for i := 0; i < n; i++ {
-		dp[i][0], dp[i][1] = 1, 1
-		for j := 0; j < i; j++ {
-			if nums[i] > nums[j] {
-				if dp[i][0] == dp[j][0]+1 {
-					dp[i][1] += dp[j][1]
-				} else if dp[i][0] < dp[j][0]+1 {
-					dp[i][0], dp[i][1] = dp[j][0]+1, dp[j][1]
-				}
+	if n == 0 {
+		return 0
+	}
+	dp := make([][][2]int, n)
+	count := 0
+	for _, num := range nums {
+		l, r := 0, count
+		for l < r {
+			mid := (l + r) / 2
+			if dp[mid][len(dp[mid])-1][0] < num {
+				l = mid + 1
+			} else {
+				r = mid
 			}
 		}
-		if max == dp[i][0] {
-			res += dp[i][1]
-		} else if max < dp[i][0] {
-			max = dp[i][0]
-			res = dp[i][1]
+		pi := l
+		paths := 1
+		if pi > 0 {
+			prev := dp[pi-1]
+			l, r = 0, len(prev)
+			for l < r {
+				mid := (l + r) / 2
+				if prev[mid][0] < num {
+					r = mid
+				} else {
+					l = mid + 1
+				}
+			}
+			paths = prev[len(prev)-1][1]
+			if l > 0 {
+				paths -= prev[l-1][1]
+			}
+		}
+		if len(dp[pi]) > 0 {
+			paths += dp[pi][len(dp[pi])-1][1]
+		}
+		dp[pi] = append(dp[pi], [2]int{num, paths})
+		if pi == count {
+			count++
 		}
 	}
-	return res
+	return dp[count-1][len(dp[count-1])-1][1]
+
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
