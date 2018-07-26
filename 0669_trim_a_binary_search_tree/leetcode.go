2d2b6b3814fe436568be48a7152e96aadb6275b5
0x5010 <0xh4cker@gmail.com>
2018-07-26 15:38:59 +0800

    0669

---
+++ 0669_trim_a_binary_search_tree/leetcode.go
@@ -0,0 +1,7 @@
+package leetcode0669
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
---
+++ 0669_trim_a_binary_search_tree/trim_a_binary_search_tree.go
@@ -0,0 +1,25 @@
+package leetcode0669
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func trimBST(root *TreeNode, L int, R int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val < L {
+		return trimBST(root.Right, L, R)
+	}
+	if root.Val > R {
+		return trimBST(root.Left, L, R)
+	}
+	root.Left = trimBST(root.Left, L, R)
+	root.Right = trimBST(root.Right, L, R)
+	return root
+}
