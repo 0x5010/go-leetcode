7145becba3cb64501c2546943b8a6e6cf33c1a0e
0x5010 <0xh4cker@gmail.com>
2018-07-30 11:06:18 +0800

    0701

---
+++ 0701_insert_into_a_binary_search_tree/insert_into_a_binary_search_tree.go
@@ -0,0 +1,24 @@
+package leetcode0701
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{
+			Val: val,
+		}
+	}
+	if val < root.Val {
+		root.Left = insertIntoBST(root.Left, val)
+	} else {
+		root.Right = insertIntoBST(root.Right, val)
+	}
+	return root
+}
