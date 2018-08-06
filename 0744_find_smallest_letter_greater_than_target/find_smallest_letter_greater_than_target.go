ed2bfdd76eed8399c8e1fa762231d4593971497d
0x5010 <0xh4cker@gmail.com>
2018-08-06 19:58:44 +0800

    0744

---
+++ 0744_find_smallest_letter_greater_than_target/find_smallest_letter_greater_than_target.go
@@ -0,0 +1,11 @@
+package leetcode0744
+
+import "sort"
+
+func nextGreatestLetter(letters []byte, target byte) byte {
+	n := len(letters)
+	i := sort.Search(n, func(i int) bool {
+		return target < letters[i]
+	})
+	return letters[i%n]
+}
