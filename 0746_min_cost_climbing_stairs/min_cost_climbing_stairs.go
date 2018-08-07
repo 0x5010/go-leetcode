bf4db35084a206d45ed6d85de65bda1d0c1cb264
0x5010 <0xh4cker@gmail.com>
2018-08-07 11:25:49 +0800

    0746

---
+++ 0746_min_cost_climbing_stairs/min_cost_climbing_stairs.go
@@ -0,0 +1,17 @@
+package leetcode0746
+
+func minCostClimbingStairs(cost []int) int {
+	n := len(cost)
+	dp := make([]int, n+1)
+	for i := 2; i <= n; i++ {
+		dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
+	}
+	return dp[n]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
