f3b2e355244c3e4a854df2167808ce8068f99d2f
0x5010 <0xh4cker@gmail.com>
2018-08-10 09:55:51 +0800

    0766

---
+++ 0766_toeplitz_matrix/toeplitz_matrix.go
@@ -0,0 +1,13 @@
+package leetcode0766
+
+func isToeplitzMatrix(matrix [][]int) bool {
+	m, n := len(matrix), len(matrix[0])
+	for i := 0; i < m-1; i++ {
+		for j := 0; j < n-1; j++ {
+			if matrix[i][j] != matrix[i+1][j+1] {
+				return false
+			}
+		}
+	}
+	return true
+}
