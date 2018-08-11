3368e52805f93fcb42c6984c1c590d30443dfe2a
0x5010 <0xh4cker@gmail.com>
2018-08-11 22:23:30 +0800

    0777

--- 0777_swap_adjacent_in_lr_string/swap_adjacent_in_lr_string.go
+++ 0777_swap_adjacent_in_lr_string/swap_adjacent_in_lr_string.go
@@ -5,20 +5,20 @@ func canTransform(start string, end string) bool {
 	if len(end) != n {
 		return false
 	}
-	for p1, p2 := 0, 0; p1 < n && p2 < n; p1, p2 = p1+1, p2+1 {
-		for p1 < n && start[p1] == 'X' {
-			p1++
+	for i, j := 0, 0; i < n && j < n; i, j = i+1, j+1 {
+		for i < n && start[i] == 'X' {
+			i++
 		}
-		for p2 < n && end[p2] == 'X' {
-			p2++
+		for j < n && end[j] == 'X' {
+			j++
 		}
-		if p1 == n && p2 == n {
+		if i == n && j == n {
 			return true
 		}
-		if p1 == n || p2 == n ||
-			start[p1] != end[p2] ||
-			start[p1] == 'L' && p2 > p1 ||
-			start[p1] == 'R' && p1 > p2 {
+		if i == n || j == n ||
+			start[i] != end[j] ||
+			start[i] == 'L' && j > i ||
+			start[i] == 'R' && i > j {
 			return false
 		}
 	}
