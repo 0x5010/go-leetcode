f3ad25e0c8008831c8ee048d41e99fbf9e822622
0x5010 <0xh4cker@gmail.com>
2018-08-14 11:38:13 +0800

    0791

--- 0791_custom-sort_string/custom-sort_string.go
+++ 0791_custom-sort_string/custom-sort_string.go
@@ -1,24 +1,22 @@
 package leetcode0791
 
-import "bytes"
-
 func customSortString(S string, T string) string {
 	count := [26]int{}
 	for i := 0; i < len(T); i++ {
 		count[T[i]-'a']++
 	}
-	bs := bytes.Buffer{}
+	bs := []byte{}
 	for i := 0; i < len(S); i++ {
 		for count[S[i]-'a'] > 0 {
-			bs.WriteByte(S[i])
+			bs = append(bs, S[i])
 			count[S[i]-'a']--
 		}
 	}
-	for b := byte('a'); b <= 'z'; b++ {
-		for count[b-'a'] > 0 {
-			bs.WriteByte(b)
-			count[b-'a']--
+	for i, c := range count {
+		for c > 0 {
+			bs = append(bs, byte(i)+'a')
+			c--
 		}
 	}
-	return bs.String()
+	return string(bs)
 }
