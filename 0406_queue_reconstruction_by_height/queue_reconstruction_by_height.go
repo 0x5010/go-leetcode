8fea2e4b15f7e0329f5f16060cd10f5de8832f02
0x5010 <0xh4cker@gmail.com>
2018-08-14 11:14:25 +0800

    0406

--- 0406_queue_reconstruction_by_height/queue_reconstruction_by_height.go
+++ 0406_queue_reconstruction_by_height/queue_reconstruction_by_height.go
@@ -3,7 +3,12 @@ package leetcode0406
 import "sort"
 
 func reconstructQueue(people [][]int) [][]int {
-	sortHW(people)
+	sort.Slice(people, func(i, j int) bool {
+		if people[i][0] == people[j][0] {
+			return people[i][1] < people[j][1]
+		}
+		return people[i][0] > people[j][0]
+	})
 	res := make([][]int, 0, len(people))
 
 	insert := func(idx int, person []int) {
@@ -18,13 +23,4 @@ func reconstructQueue(people [][]int) [][]int {
 		insert(p[1], p)
 	}
 	return res
-}
-
-func sortHW(people [][]int) {
-	sort.Slice(people, func(i, j int) bool {
-		if people[i][0] == people[j][0] {
-			return people[i][1] < people[j][1]
-		}
-		return people[i][0] > people[j][0]
-	})
 }
