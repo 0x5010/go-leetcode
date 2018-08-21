cdfba910b7f50da9da20e9abbe2ff9985a00b9a1
0x5010 <0xh4cker@gmail.com>
2018-08-21 15:08:00 +0800

    0814

---
+++ 0814_binary_tree_pruning/binary_tree_pruning.go
@@ -0,0 +1,24 @@
+package leetcode0814
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func pruneTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	root.Left = pruneTree(root.Left)
+	root.Right = pruneTree(root.Right)
+
+	if root.Val == 0 && root.Left == nil && root.Right == nil {
+		return nil
+	}
+	return root
+}
---
+++ 0814_binary_tree_pruning/leetcode.go
@@ -0,0 +1,7 @@
+package leetcode0814
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
