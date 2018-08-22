c1326a09df2d49bf1a164b0ca4514a9276be9fd9
0x5010 <0xh4cker@gmail.com>
2018-08-22 11:28:54 +0800

    0804

--- 0804_unique_morse_code_words/unique_morse_code_words.go
+++ 0804_unique_morse_code_words/unique_morse_code_words.go
@@ -6,7 +6,7 @@ func uniqueMorseRepresentations(words []string) int {
 	t := [26]string{".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."}
 	m := map[string]struct{}{}
 	for _, word := range words {
-		var bs strings.Builder
+		bs := strings.Builder{}
 		for i := 0; i < len(word); i++ {
 			bs.WriteString(t[word[i]-'a'])
 		}
