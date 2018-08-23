f5be3d657012cc79cdeaa2c55f89cb83d2340a7b
0x5010 <0xh4cker@gmail.com>
2018-08-23 11:11:47 +0800

    0482

--- 0482_license_key_formatting/license_key_formatting.go
+++ 0482_license_key_formatting/license_key_formatting.go
@@ -1,26 +1,20 @@
 package leetcode0482
 
-import (
-	"bytes"
-	"strings"
-)
-
 func licenseKeyFormatting(S string, K int) string {
-	b := bytes.Buffer{}
+	bs := []byte{}
 	for i := len(S) - 1; i >= 0; i-- {
-		if S[i] == '-' {
+		b := S[i]
+		if b == '-' {
 			continue
 		}
-		if b.Len()%(K+1) == K {
-			b.WriteByte('-')
+		if len(bs)%(K+1) == K {
+			bs = append(bs, '-')
 		}
-		b.WriteByte(S[i])
+		if b >= 'a' {
+			b -= 32
+		}
+		bs = append(bs, b)
 	}
-
-	return strings.ToUpper(reverse(b.Bytes()))
-}
-
-func reverse(bs []byte) string {
 	i, j := 0, len(bs)-1
 	for i < j {
 		bs[i], bs[j] = bs[j], bs[i]
