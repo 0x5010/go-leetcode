8366df98a3f3b67a9794d80e26f10c712ab7c62d
0x5010 <0xh4cker@gmail.com>
2018-09-04 20:44:37 +0800

    0867

---
+++ 0867_transpose_matrix/transpose_matrix.go
@@ -0,0 +1,15 @@
+package leetcode0867
+
+func transpose(A [][]int) [][]int {
+	m, n := len(A), len(A[0])
+	res := make([][]int, n)
+	for i := 0; i < n; i++ {
+		res[i] = make([]int, m)
+	}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			res[j][i] = A[i][j]
+		}
+	}
+	return res
+}
