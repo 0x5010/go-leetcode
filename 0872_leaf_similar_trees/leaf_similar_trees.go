c277da65760861857ca281357878af06ecc8a584
0x5010 <0xh4cker@gmail.com>
2018-09-06 21:26:44 +0800

    0872

---
+++ 0872_leaf_similar_trees/leaf_similar_trees.go
@@ -0,0 +1,36 @@
+package leetcode0872
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func leafSimilar(root1 *TreeNode, root2 *TreeNode) bool {
+	s1, s2 := []*TreeNode{root1}, []*TreeNode{root2}
+	var dfs func(*[]*TreeNode) int
+	dfs = func(s *[]*TreeNode) int {
+		for {
+			node := (*s)[len(*s)-1]
+			*s = (*s)[:len(*s)-1]
+			if node.Left != nil {
+				*s = append(*s, node.Left)
+			}
+			if node.Right != nil {
+				*s = append(*s, node.Right)
+			}
+			if node.Left == nil && node.Right == nil {
+				return node.Val
+			}
+		}
+	}
+	for len(s1) != 0 && len(s2) != 0 {
+		if dfs(&s1) != dfs(&s2) {
+			return false
+		}
+	}
+	return len(s1) == 0 && len(s2) == 0
+}
---
+++ 0872_leaf_similar_trees/leetcode.go
@@ -0,0 +1,7 @@
+package leetcode0872
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
