e4f2bce671c60462c2a1b0d4c239662b5b9b53f8
0x5010 <0xh4cker@gmail.com>
2018-09-07 11:31:04 +0800

    0872

--- 0872_leaf_similar_trees/leaf_similar_trees.go
+++ 0872_leaf_similar_trees/leaf_similar_trees.go
@@ -11,26 +11,26 @@ package leetcode0872
 
 func leafSimilar(root1 *TreeNode, root2 *TreeNode) bool {
 	s1, s2 := []*TreeNode{root1}, []*TreeNode{root2}
-	var dfs func(*[]*TreeNode) int
-	dfs = func(s *[]*TreeNode) int {
-		for {
-			node := (*s)[len(*s)-1]
-			*s = (*s)[:len(*s)-1]
-			if node.Left != nil {
-				*s = append(*s, node.Left)
-			}
-			if node.Right != nil {
-				*s = append(*s, node.Right)
-			}
-			if node.Left == nil && node.Right == nil {
-				return node.Val
-			}
-		}
-	}
 	for len(s1) != 0 && len(s2) != 0 {
 		if dfs(&s1) != dfs(&s2) {
 			return false
 		}
 	}
 	return len(s1) == 0 && len(s2) == 0
+}
+
+func dfs(s *[]*TreeNode) int {
+	for {
+		node := (*s)[len(*s)-1]
+		*s = (*s)[:len(*s)-1]
+		if node.Left != nil {
+			*s = append(*s, node.Left)
+		}
+		if node.Right != nil {
+			*s = append(*s, node.Right)
+		}
+		if node.Left == nil && node.Right == nil {
+			return node.Val
+		}
+	}
 }
