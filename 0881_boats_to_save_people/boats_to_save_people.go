89fd7a48aca986497ef79b4412ec9d04c6d4f600
0x5010 <0xh4cker@gmail.com>
2018-09-11 11:01:36 +0800

    0881

--- 0881_boats_to_save_people/boats_to_save_people.go
+++ 0881_boats_to_save_people/boats_to_save_people.go
@@ -5,13 +5,12 @@ import "sort"
 func numRescueBoats(people []int, limit int) int {
 	n := len(people)
 	sort.Ints(people)
-	l, r := 0, n-1
+
 	res := 0
-	for l <= r {
+	for l, r := 0, n-1; l <= r; r-- {
 		if people[l]+people[r] <= limit {
 			l++
 		}
-		r--
 		res++
 	}
 	return res
