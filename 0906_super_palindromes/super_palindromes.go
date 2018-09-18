e75e994b34dcc7c947281ec1cc09619d10fa3251
0x5010 <0xh4cker@gmail.com>
2018-09-18 18:57:04 +0800

    0906

--- 0906_super_palindromes/super_palindromes.go
+++ 0906_super_palindromes/super_palindromes.go
@@ -11,10 +11,11 @@ func superpalindromesInRange(L string, R string) int {
 
 	left := int(math.Sqrt(float64(l)))
 	right := int(math.Sqrt(float64(r)))
+	ll, lr := len(strconv.Itoa(left)), len(strconv.Itoa(right))
 
 	res := 0
 L:
-	for n := 1; n < 19; n++ {
+	for n := ll; n < lr+1; n++ {
 		for _, p := range genP(n)[1:] {
 			if p < left {
 				continue
@@ -30,18 +31,22 @@ L:
 	return res
 }
 
+var cacheP = [18][]int{nil, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, {0, 11, 22, 33, 44, 55, 66, 77, 88, 99}}
+
 func genP(n int) []int {
 	if n == 0 {
 		return nil
 	}
-	if n == 1 {
-		return []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	res := cacheP[n]
+	if len(res) != 0 {
+		return cacheP[n]
 	}
-	if n == 2 {
-		return []int{0, 11, 22, 33, 44, 55, 66, 77, 88, 99}
+
+	sub := cacheP[n-2]
+	if len(sub) == 0 {
+		sub = genP(n - 2)
 	}
-	sub := genP(n - 2)
-	res := []int{}
+	res = []int{}
 	for i := 0; i < 10; i++ {
 		for _, s := range sub {
 			r := i
@@ -51,6 +56,7 @@ func genP(n int) []int {
 			res = append(res, r+s*10+i)
 		}
 	}
+	cacheP[n] = res
 	return res
 }
 
