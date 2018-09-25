8372fa85237978c718a43105f07820c12ef3c1a5
0x5010 <0xh4cker@gmail.com>
2018-09-25 19:12:21 +0800

    0035

--- 0035_search_insert_position/search_insert_position.go
+++ 0035_search_insert_position/search_insert_position.go
@@ -3,9 +3,6 @@ package leetcode0035
 func searchInsert(nums []int, target int) int {
 	i := 0
 	for i < len(nums) && nums[i] <= target {
-		if nums[i] == target {
-			return i
-		}
 		i++
 	}
 	return i
