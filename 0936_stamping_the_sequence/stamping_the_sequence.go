883b433fb95835d9cf1ecb132d599187a64c0c1d
0x5010 <0xh4cker@gmail.com>
2018-11-20 11:33:04 +0800

    0936

---
+++ 0936_stamping_the_sequence/stamping_the_sequence.go
@@ -0,0 +1,38 @@
+package leetcode0936
+
+func movesToStamp(stamp string, target string) []int {
+	m, n := len(stamp), len(target)
+	count := 0
+	bStamp, bTarget := []byte(stamp), []byte(target)
+	res := []int{}
+L:
+	for count < n {
+		index := 0
+		for index = 0; index <= n-m; index++ {
+			found := false
+			var i, j int
+			for i, j = index, 0; j < m; i, j = i+1, j+1 {
+				if bTarget[i] != '*' && bTarget[i] != bStamp[j] {
+					break
+				} else if bTarget[i] == bStamp[j] {
+					found = true
+				}
+			}
+			if j == m && found {
+				for k := index; k < index+m; k++ {
+					if bTarget[k] != '*' {
+						bTarget[k] = '*'
+						count++
+					}
+				}
+				res = append(res, index)
+				continue L
+			}
+		}
+		return []int{}
+	}
+	for i, j := 0, len(res)-1; i < j; i, j = i+1, j-1 {
+		res[i], res[j] = res[j], res[i]
+	}
+	return res
+}
